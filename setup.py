59d0abb9df0eb4b11719f9f51ef918afb9d1083e
Angga Dwi Sunarto <kanshinbatosai@gmail.com>
2024-11-05 21:02:58 +0700

    setup.py [update]

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'trash_classification_ml_project',
+    version= '0.0.0',
+    author= 'Angga Dwi Synarto',
+    author_email= 'anggadwisunarto3@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+)
